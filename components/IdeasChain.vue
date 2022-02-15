7b24bde96982a822dc3ca67b2d643cfb5431e04e
lcarbonn <laurent.carbonnaux@gmail.com>
2022-02-15 11:30:28 +0100

    one player game play

--- components/IdeasChain.vue
+++ components/IdeasChain.vue
@@ -1,17 +1,30 @@
 <!-- Game page -->
 <template>
   <div>
+    <div>
     <IdeaCard v-for="idea in ideas"
               :key="idea.message"
               :idea="idea"/>
+    </div>
+    <div>
+      <md-button class="md-primary" @click="back2Game">Back to the game</md-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IdeaChain',
-    props: {
-        ideas:[]
-    }
+
+  props: {
+      ideas:[]
+  },
+
+  methods: {
+    back2Game() {
+      this.$emit('back2Game')
+    },
+  }
+
 }
 </script>
--- components/PlayerGame.vue
+++ components/PlayerGame.vue
@@ -2,44 +2,51 @@
 <template>
   <div>
     <md-field v-if="lastIdea">
-      <md-input disable v-model="lastIdea">{{lastIdea}}</md-input>
+      <md-input disable v-model="lastIdea">{{ lastIdea }}</md-input>
     </md-field>
     <md-field>
       <label v-if="!lastIdea">Please, write the first idea here...</label>
       <label v-else>Please, write your idea here...</label>
-      <md-input v-model="newIdea"></md-input>
+      <md-input v-model="newIdea" @keydown.enter="addNewIdea"></md-input>
     </md-field>
-    <md-button class="md-primary" @click="addNewIdea">Send to next player</md-button>
-    <span class="md-primary">{{size}}</span>
+    <md-button class="md-primary" @click="addNewIdea"
+      >Send to next player</md-button
+    >
+    <span class="md-primary">{{ size }}</span>
     <md-button class="md-primary" @click="endGame">End the game</md-button>
-   </div>
+    <md-button class="md-primary" @click="resetGame">Reset game</md-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'GamePage',
+  name: "GamePage",
 
   data: () => ({
     lastIdea: "",
-    newIdea:"",
-    size:"",
-    ideas: []
+    newIdea: "",
+    size: "",
+    ideas: [],
   }),
-  
+
   methods: {
     addNewIdea() {
-      if(this.newIdea!="") {
-          this.ideas.push({message: this.newIdea}),
-          this.lastIdea = this.newIdea,
-          this.newIdea = ""
-          this.size=this.ideas.length
-        }
-        //this.$store.dispatch("idea/saveIdea", this.newIdea)
+      if (this.newIdea != "") {
+        this.ideas.push({ message: this.newIdea }),
+          (this.lastIdea = this.newIdea),
+          (this.newIdea = "");
+        this.size = this.ideas.length;
+      }
+      //this.$store.dispatch("idea/saveIdea", this.newIdea)
     },
     endGame() {
-      console.log(this.ideas),
-      this.$emit('endGame', this.ideas)
+      console.log(this.ideas), this.$emit("endGame", this.ideas);
     },
-  }
-}
+    resetGame() {
+      (this.ideas = []), (this.lastIdea = ""), (this.newIdea = "");
+      this.size = "";
+      this.$emit("resetGame");
+    },
+  },
+};
 </script>
