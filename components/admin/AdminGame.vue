cd9096fff2048b6604791102b99d5bcdbdff538b
lcarbonn <laurent.carbonnaux@gmail.com>
2022-02-23 18:23:28 +0100

    open join in blank target

--- components/admin/AdminGame.vue
+++ components/admin/AdminGame.vue
@@ -5,14 +5,14 @@
       <span>Copy/Paste the link and share it to players</span>
     </div>
     <div>
-      <nuxt-link v-if="game" :to="'/game/' + game.id">{{gamePath}}</nuxt-link>
+      <NuxtLink v-if="game" :to="'/game/' + game.id" target="_blank">{{gamePath}}</NuxtLink>
     </div>
     <br/>
     <div>
       <span>Players in the game : {{nbPlayers}}</span>
     </div>
     <div>
-      <md-button v-if="game" class="md-primary" :to="'/game/' + game.id">Join the game</md-button>
+      <md-button v-if="game" class="md-primary" :to="'/game/' + game.id" target="_blank">Join the game</md-button>
       <md-button :disabled="this.started" class="md-primary" @click="startGame">Start the game</md-button>
       <md-button :disabled="!this.started" class="md-primary" @click="endGame">End the game</md-button>
     </div>
