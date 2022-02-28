968c01939002b4dc8cb8619b12355df3a02c28ed
lcarbonn <laurent.carbonnaux@gmail.com>
2022-02-28 19:45:24 +0100

    change hid

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,11 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'idealicious',
+    title: 'Idealicious',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Generate great ideas with Idealicious' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
