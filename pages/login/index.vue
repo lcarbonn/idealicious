4bcfdf8779bf9f5773180e114183e77e6dbcd5cc
lcarbonn <laurent.carbonnaux@gmail.com>
2023-03-05 11:16:54 +0100

    replace label by placeholder

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,30 +3,22 @@
         <b-card-text>
             <b-container fluid>
                 <b-row class="my-1">
-                    <b-col sm="2">
-                    <label for="email">Email</label>
-                    </b-col>
-                    <b-col sm="10">
-                    <b-form-input id="email" v-model="form.email" type="email" :state="emailState"></b-form-input>
+                    <b-col>
+                    <b-form-input id="email" placeholder="Email" v-model="form.email" type="email" :state="emailState"></b-form-input>
                     </b-col>
                 </b-row>
                 <b-row class="my-2">
-                    <b-col sm="2">
-                    <label for="password">Password</label>
-                    </b-col>
-                    <b-col sm="10">
-                    <b-form-input id="password" v-model="form.password" type="password" :state="passwordState"></b-form-input>
+                    <b-col>
+                    <b-form-input id="password" placeholder="Password" v-model="form.password" type="password" :state="passwordState"></b-form-input>
                     </b-col>
                 </b-row>
                 <b-row class="my-2" align-h="center">
-                    <b-col sm="2"></b-col>
-                    <b-col sm="10">
+                    <b-col>
                         <b-button :disabled="disabledButton" block @click="emailLogin()">Connexion</b-button>
                     </b-col>
                 </b-row>
                 <b-row class="my-3" align-h="center">
-                    <b-col sm="2"></b-col>
-                    <b-col sm="10">
+                    <b-col>
                         <p><nuxt-link to="/login/reset-password">J'ai oublié mon mot de passe :'(</nuxt-link></p>
                         <!-- <p>Vous n'avez pas de compte ? Vous pouvez <nuxt-link to="/signup">en créer un</nuxt-link>.</p> -->
                     </b-col>
